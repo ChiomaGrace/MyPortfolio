33ecebcd9e97864ab4bcf0dfee46afd77cfda153
Chioma Ubogagu <chioma.ubogagu@simpleviewinc.com>
2023-09-09 20:49:41 -0700

    changed port syntax

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express'); //a routing and middleware (functions that have access to request/response objects) web framework that has minimal functionality of its own and is essentially a series of middleware function calls. must require('express') to use the package before attempting to invoke it,otherwise an error 'express is not defined' will occur
 const app = express(); //this is the function that then invokes express
 const mongoose = require('mongoose');
-const port = process.env.NODE_PORT || 3000; //the port the node server will run on
+// const port = process.env.NODE_PORT || 3000; //the port the node server will run on
+const port = process.env.PORT || 3000; //changed for deployment
 const routes = require('./server/config/routes'); //linking the routes file
 
 
