f0a4309b8ac44472d96aef7427aaa2b676b5e368
Chioma Ubogagu <chioma.ubogagu@simpleviewinc.com>
2023-09-09 22:05:07 -0700

    removing express static line

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000; //changed for deployment
 const routes = require('./server/config/routes'); //linking the routes file
 
 
-var myPortfolioDatabase = mongoose.connect(process.env.MONGO_URL, function(error, response) { //process.env.MONGO_URL is the variable created in railway deployment for my service. otherwise monngo db url from atlas can go there for local testing
+var myPortfolioDatabase = mongoose.connect(process.env.MONGO_URL, function(error, response) { //process.env.MONGO_URL is the variable created in railway deployment for my service
   if(error) {
     console.log("This error is from the server.js file where mongoose is connecting:", error);
   }
@@ -20,7 +20,7 @@ var myPortfolioDatabase = mongoose.connect(process.env.MONGO_URL, function(error
 
 app.use(express.json()); // middleware that parses incoming requests with JSON payloads (HTTP post, put, patch requests) and is based on body-parser aka so you can see/work with form data
 app.use(express.urlencoded()); // middleware that parses incoming requests with URL-ecnoded payloads (HTTP post, put, patch requests) and is based on body-parser aka so you can see/work with form data
-app.use('/', express.static('dist/my-portfolio')) // this connects/serves static files
+// app.use('/', express.static('dist/my-portfolio')) // this connects/serves static files locally not needed for deployment
 app.use('/', routes);  //this invokes the routes.js file
 
 app.listen(port, () => { //starts a port and hosts connection
