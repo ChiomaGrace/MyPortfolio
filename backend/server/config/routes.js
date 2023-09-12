dc9d89cb3aafa4c26022af04d2add587f1d5abbf
Brody Over <brodyover@gmail.com>
2023-09-11 22:03:20 -0400

    refactoring

--- backend/server/config/routes.js
+++ backend/server/config/routes.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const router = express.Router(); //links the route call on the server.js
 const emailController = require("../controllers/emails"); //links the email controller which has all the functions
-import { environment } from '../environments/environment';
 
+router.post('/sendEmail', emailController.createEmail);
 
-    // router.post('/sendEmail', emailController.createEmail); 
-
-    router.post(environment.backendURL + '/sendEmail' , emailController.createEmail); environment
-
-    router.get('/test', (request, response) => {
-        response.send("This is a test response from routes.js")
-        console.log("This console log is coming from the route.js")
-    })
-
-    
-
+router.get('/test', (request, response) => {
+    response.send("This is a test response from routes.js")
+    console.log("This console log is coming from the route.js")
+});
 
 module.exports = router; //helps to achieve module programming. Modular programming refers to separating the functionality of a program into independent, interchangeable modules, such that each contains everything necessary to execute only one aspect of the desired functionality
