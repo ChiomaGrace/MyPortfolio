3d426aee4d4aeebc7a15b6bbf73249a217ffe4c0
Brody Over <brodyover@gmail.com>
2023-09-11 23:25:39 -0400

    use cors

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express'); //a routing and middleware (functions that have access to request/response objects) web framework that has minimal functionality of its own and is essentially a series of middleware function calls. must require('express') to use the package before attempting to invoke it,otherwise an error 'express is not defined' will occur
+const cors = require('cors');
 const app = express(); //this is the function that then invokes express
 const mongoose = require('mongoose');
 const routes = require('./server/config/routes'); //linking the routes file
@@ -12,6 +13,10 @@ var myPortfolioDatabase = mongoose.connect(process.env.MONGO_URL, function (erro
     // console.log(response);
   }
 });
+
+app.use(cors({
+  origin: [process.env.FRONTEND_ORIGIN, "http://127.0.0.1:4200", "http://localhost:4200"]
+}));
 
 app.use(express.json()); // middleware that parses incoming requests with JSON payloads (HTTP post, put, patch requests) and is based on body-parser aka so you can see/work with form data
 
