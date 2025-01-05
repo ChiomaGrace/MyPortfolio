82a63c0beb1b690384e71703bdfebd50f3dc6251
Chioma Ubogagu <cubogagu10@gmail.com>
2025-01-05 00:33:35 -0600

    Clean Up DB error
    solving mongoose connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express'); //a routing and middleware (functions that have access to request/response objects) web framework that has minimal functionality of its own and is essentially a series of middleware function calls. must require('express') to use the package before attempting to invoke it,otherwise an error 'express is not defined' will occur
-const cors = require('cors');
 const app = express(); //this is the function that then invokes express
+const cors = require("cors"); //needed in order to setup local and deployed environments
+const dotenv = require("dotenv"); //needed in order to use .env for database
+dotenv.config();
 const mongoose = require('mongoose');
 const routes = require('./server/config/routes'); //linking the routes file
 
